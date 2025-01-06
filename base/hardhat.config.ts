ff1fea254cc66ff6ae5dcca56ef769a9557801b3
cunnily <botalovtim@gmail.com>
2025-01-06 13:52:51 +0300

    changed network from Arbitrum to Base

--- base/hardhat.config.ts
+++ base/hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+  networks: {
+    base: {
+      url: `https://base-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`${process.env.PRIVATE_KEY}`], 
+    },
+  }
 };
 
 export default config;
